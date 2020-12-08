9d4df6a847454a30a56d9d55ec61eb8ceddc3ff3
kris <kristmitina@gmail.com>
2020-12-08 13:33:31 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by DVKSH on 8.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
